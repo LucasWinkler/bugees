2bf888ea73b4acc7c9e1c783388f7cf86f4ddb5d
Lucas Winkler <lucaswinkler@gmail.com>
2022-11-24 21:52:49 -0600

    Possibly fixed colour contrast issues for CTA and contact section on landing page

--- src/components/home/Contact/index.jsx
+++ src/components/home/Contact/index.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Contact() {
   return (
-    <section id='contact' className='bg-green-500 py-14'>
+    <section id='contact' className='bg-green-600 py-14'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center flex gap-1 flex-col'>
         <h2 className='text-white text-center font-bold text-3xl md:text-4xl xl:text-5xl'>
           Got any more questions?
